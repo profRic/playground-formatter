50890742c9ebb6c78e0fdf2ff258890a908ff648
rt113@duke.edu <rt113@duke.edu>
2017-03-12 11:14:47 -0400

    minor change to Common file

---
+++ TestRTF/CommonObjects.swift
@@ -0,0 +1,25 @@
+//
+//  CommonObjects.swift
+//  TestRTF
+//
+//  Created by Ric Telford on 3/11/17.
+//  Copyright © 2017 Ric Telford. All rights reserved.
+//
+
+import UIKit
+
+//  Supported objects so far
+
+enum markupType {
+    case text
+    case italic
+    case bold
+    case h1
+    case h2
+    case h3
+    case bullet
+    case num
+    case code
+}
+
+
